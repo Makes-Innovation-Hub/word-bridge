95dc524c1a0fb324a36d74967164064eb9b406dc
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-07-14 13:49:55 +0300

    menubar

--- src/Components/MenuBar.jsx
+++ src/Components/MenuBar.jsx
@@ -1,5 +1,18 @@
+import React from "react";
+// import { Box_Orange_Rounded } from "../assets/Box_Orange_Rounded.png";
+
 function MenuBar() {
-  return <div>MenuBar</div>;
+  return (
+    <div>
+      {/* <Box_Orange_Rounded /> */}
+
+      <img
+        className="h-40 w-container"
+        src={`./img/Box_Orange_Rounded.png`}
+        alt=""
+      />
+    </div>
+  );
 }
 
 export default MenuBar;
