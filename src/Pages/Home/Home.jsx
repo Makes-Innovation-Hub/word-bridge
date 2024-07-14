581b13681853c1b90e0d97680ab91e826c3d4bad
Artur Abel <163734294+ArturAbel@users.noreply.github.com>
2024-07-14 22:44:51 +0300

    main page polish

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,22 +9,33 @@ export default function Home() {
     <div id="root">
       <video autoPlay muted loop id="background-video">
         <source src={backgroundVideo} type="video/mp4" />
-        
       </video>
       <div className="App">
-        <div className="home-container h-full">
+        <div className="home-container">
           <header className="App-header">
-            <h1 className="title" style={{ position: "absolute" }}>
-              Alphabets Game
-            </h1>
+            <svg className="title" viewBox="0 0 500 500">
+              <path
+                id="curve"
+                d="M55.2,140.6c4-6.1,40-46.8,210.6-50.6c150.3,1.2,150.8,60,350.1,100"
+              />
+              <text>
+                <textPath xlinkHref="#curve">The Alphabet Game</textPath>
+              </text>
+            </svg>
             <img className="img-logo" src={orange} alt="Logo" />
           </header>
           <div className="button-container">
-            <button className="mr-10" onClick={() => navigate("/Alphabatics")}>
-              Alphabets
+            <button
+              className="main-page-btn letters-btn"
+              onClick={() => navigate("/Alphabatics")}
+            >
+              Letters
             </button>
-            <button className="mr-9" onClick={() => navigate("/words")}>
-              words
+            <button
+              className="main-page-btn words-btn"
+              onClick={() => navigate("/words")}
+            >
+              Words
             </button>
           </div>
         </div>
