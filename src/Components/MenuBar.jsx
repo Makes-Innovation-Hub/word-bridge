bbcd9c47a17efda9f424faf92063fe1ba24ac44b
Artur Abel <163734294+ArturAbel@users.noreply.github.com>
2024-07-15 01:11:29 +0300

    design, cards responsivness

--- src/Components/MenuBar.jsx
+++ src/Components/MenuBar.jsx
@@ -2,16 +2,18 @@ import ButtonText_Small_Orange_Square from "../assets/ButtonText_Small_Orange_Sq
 import Timer from "./Timer";
 import Scores from "./Scores";
 
+import "./MenuBar.css";
+
 function MenuBar({ constTime, timeLeft, setTimeLeft }) {
   return (
     <div className="h-auto w-container">
       <ButtonText_Small_Orange_Square>
+      <div className="menuBar-logo mt-6">word bridge</div>
         <div className="flex flex-col w-full h-full gap-1 items-center justify-between">
-          <div className="mt-6">logos</div>
-          <div className="w-full">
+          <div className="menuBar-score-container w-full">
             <Scores />
           </div>
-          <div className="w-9/12 mb-12 ">
+          <div className="menuBar-timer w-9/12 mb-12 ">
             <Timer
               constTime={constTime}
               setTimeLeft={setTimeLeft}
