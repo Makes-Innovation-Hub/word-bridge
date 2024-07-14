0dcea3d105be4cc068625980a5a4f7f692de5152
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-07-14 22:05:50 +0300

    timer and starting the score

--- src/Components/Scores.jsx
+++ src/Components/Scores.jsx
@@ -9,25 +9,27 @@ function Scores() {
           <p>10</p>
         </div>
       </div>
-      <div className="h-full flex flex-row  justify-between ">
-        <div className="relative w-16 h-10">
-          <img
-            src="../../img/star.png"
-            className="absolute top-0 left-1/2 transform -translate-x-1/2 h-6"
-            alt="Star 1"
-          />
-          <img
-            src="../../img/star.png"
-            className="absolute bottom-0 left-0 h-6"
-            alt="Star 2"
-          />
-          <img
-            src="../../img/star.png"
-            className="absolute bottom-0 right-0 h-6"
-            alt="Star 3"
-          />
+      <div className="h-full flex flex-row bg-orange-300 px-4 justify-between rounded-lg border-4 border-orange-900 relative">
+        <div className="absolute left-0 top-1/2 transform -translate-y-1/2">
+          <div className="relative w-16 h-10 flex-shrink-0">
+            <img
+              src="../../img/star.png"
+              className="absolute -top-4 h-6 left-0  "
+              alt="Star 1"
+            />
+            <img
+              src="../../img/star.png"
+              className="absolute -top-4 right-0  h-6 "
+              alt="Star 2"
+            />
+            <img
+              src="../../img/star.png"
+              className="absolute -top-4 right-5 h-6 "
+              alt="Star 3"
+            />
+          </div>
         </div>
-        <div className="text-lg h-10 flex items-center justify-center text-center">
+        <div className="text-lg h-10 flex items-center justify-center text-center flex-grow">
           <p>100</p>
         </div>
       </div>
